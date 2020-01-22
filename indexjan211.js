e7ae7039919a7bc2bcf236356398c54dbb1f2be0
yadwinder123 <59842356+yadwinder123@users.noreply.github.com>
2020-01-22 12:24:42 -0700

    Add files via upload

---
+++ indexjan211.js
@@ -0,0 +1,35 @@
+function divide(num)
+{
+  return 12000 / num * 2000
+}
+console.log(divide(10));
+
+function multiplies(x,y,z,q)
+{
+  return x*y*z*q
+
+}
+console.log(multiplies(2,4,6,8));
+
+function colorname(x)
+{
+  return "red";
+
+}
+console.log(colorname("red"))
+
+function addition(x,y,z)
+{
+ return x + y * z ; 
+}
+console.log(addition(12,13,114));
+
+function relationship(brother,sister){
+  return brother + " is playing with his " + sister;
+}
+console.log(relationship("brother","sister"))
+
+function friends(mike,roger){
+  return mike + " and " + roger + " are brother from another mother " ;
+}
+console.log(friends("mike" , "roger"));
