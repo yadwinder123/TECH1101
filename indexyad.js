1391d39564e417673c7ec58a1e66c50f5533de78
yadwinder123 <59842356+yadwinder123@users.noreply.github.com>
2020-01-27 09:30:55 -0700

    Add files via upload

---
+++ indexyad.js
@@ -0,0 +1,34 @@
+function division(value1,value2)
+{
+    return value1/value2
+}
+console.log(division(123456789,9));
+
+function substraction(p1,p2)
+
+{return p2-p1;
+
+
+}
+ console.log(substraction(700,900);)
+
+
+ function sum(n1,n2)
+{
+    return n1+ "+" +n2'
+}
+console.log(sum("brother" , "sister"));
+
+var dog={
+    name : 'jakk',
+    breed : 'german',
+    age : '8 months',
+    weight : '40kg',
+    colour : 'white',
+}
+console.log(dog);
+
+function addition(term1,term2){
+    return term1+term2;
+}
+console.log(addition(1000,2000));
